96900168b07815720c6c27c8c9f04ddc59e1cff5
Elham <elham.khani@newday.co.uk>
2023-11-27 22:55:32 +0000

    add loader

--- pages/components/loading.js
+++ pages/components/loading.js
@@ -1,3 +1,15 @@
+import { ThreeCircles } from "react-loader-spinner"
 export default function Loading() {
-    return <p>Loading...</p>
+    return <ThreeCircles
+    height="100"
+    width="100"
+    color="#10a37f"
+    wrapperStyle={{}}
+    wrapperClass=""
+    visible={true}
+    ariaLabel="three-circles-rotating"
+    outerCircleColor=""
+    innerCircleColor=""
+    middleCircleColor=""
+  />
   }
